bff0413173b8e7f272827d3d8b04b991e447a0a0
Bruno Alves <vespidhook@gmail.com>
2022-06-19 02:41:58 -0300

    docker docker-compose

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,7 +1,9 @@
+import { Request, Response } from 'express';
+
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
-    constructor(private importCategoryUseCase: ImportCategoryUseCase);
+    constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
     handle(request: Request, response: Response): Response {
         const { file } = request;
 
