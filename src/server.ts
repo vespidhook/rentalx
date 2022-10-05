2ea27a09e1566a902cb1017911a4f708f2f9d101
Bruno Alves <basilva@unisuam.edu.br>
2022-10-05 20:09:55 -0300

    categories

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({ message:"Hello World"});
-});
-
-app.post("/courses", (request, response) => {
-    const { name }  = request.body;
-
-    return response.json({ name });
-})
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running'));
