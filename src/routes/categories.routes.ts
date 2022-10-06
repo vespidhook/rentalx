db0082c3df720c607099ba797de3600b1225cbc8
Bruno Alves <basilva@unisuam.edu.br>
2022-10-05 22:37:40 -0300

    validacao

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,22 @@
-import { Router } from 'express'; 
-import { Category } from '../model/Category';
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
-
-categoriesRoutes.post("/", (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
 
-    const category = new Category();
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    });
+    categoriesRepository.create({ name, description });
 
-    categories.push(category);
+    return response.status(201).send();
+});
 
-    return response.status(201).json(category);
+categoriesRoutes.get('/', (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
 });
 
 export { categoriesRoutes };
