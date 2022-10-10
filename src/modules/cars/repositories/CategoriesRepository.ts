baecae1af5bd6f93a7177fff00950fbfcf8cb448
Bruno Alves <basilva@unisuam.edu.br>
2022-10-10 08:42:23 -0300

    parametro disponibilidade

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,8 +7,14 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
 
-    constructor() {
+    private static INSTANCE: CategoriesRepository;
+
+    private constructor() {
         this.categories = [];
+    }
+
+    public static getInstance(): CategoriesRepository {
+        if  
     }
 
     create({ name, description }: ICreateCategoryDTO): void {
